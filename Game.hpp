3f5238db3aed8f1ce78332dc77591310265fc6d0
Arisha75144 <88162494+Arisha75144@users.noreply.github.com>
2022-05-31 18:52:45 +0300

    Update Game.hpp

--- Game.hpp
+++ Game.hpp
@@ -10,10 +10,16 @@ class Game {
 public:
     Game (); // default constructor
     ~Game (); // destructor
+    void displayBoard(); //displays our board 
+    void getMove(char player); //asks the user for his move
+    
 
     void Start (); // Game entry point
 private:
+    const int MaxMoves=9;
     int *Array; // array of cells
+    bool checkForWins(char player) //check wins
+    bool checkForDraw() //check request limits
 
 
 };
