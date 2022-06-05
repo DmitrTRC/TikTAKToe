db95a68a6f106211584d0586932eaf283deae353
Arisha75144 <88162494+Arisha75144@users.noreply.github.com>
2022-06-05 18:01:23 +0300

    Create player.hpp

---
+++ player.hpp
@@ -0,0 +1,22 @@
+//
+// Created by Dmitry Morozov on 2/6/22.
+//
+
+#ifndef TIKTAKTOE_PLAYER_HPP
+#define TIKTAKTOE_PLAYER_HPP
+
+
+class Player {
+    Public:
+    Player();  
+    std::string Name;     
+
+
+
+
+
+
+};
+
+
+#endif //TIKTAKTOE_PLAYER_HPP
