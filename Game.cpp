243a0f95de24355629ccf0f79581e76cf76ef1b2
Arisha75144 <88162494+Arisha75144@users.noreply.github.com>
2022-06-05 17:57:22 +0300

    Update Game.cpp

--- Game.cpp
+++ Game.cpp
@@ -4,8 +4,9 @@
 #include <iostream>
 
 #include "Game.hpp"
+#include "Player.hpp"
 
-
+//TODO: Fix the code below
 Game::Game () {
 
     std::cout << "class Game constructor is running" << std::endl;
@@ -19,4 +20,50 @@ Game::~Game () {
 
 void Game::Start () {
     std::cout << "Game started" << std::endl;
+    std::string name = InputUserName ();
+
+    auto db = LoadDataBase ();
+    Player player1 = FindPlayer (db, name);
+    Player player2; // Constructor is not defined
+
+// Repeating the game until the user quits
+    while (is_playing ()) {
+        bool player_type = GetOpponentType ();
+        SetSecondPlayer (player_type);
+
+        if (player_type) {
+            player2.Name = "Computer";
+        } else {
+            player2.Name = InputUserName ();
+        }
+
+        Loop ();
+
+        IsPlayAgain ();
+// End of the Game
+    }
+
 }
+
+
+std::string Game::InputUserName () {
+    std::cout << "Enter User name running and returns a string Dmitry" << std::endl;
+    return std::string ("Dmitry");
+}
+
+std::string Game::LoadDataBase(){
+    std::cout << "DataBase" << std::endl;
+    return std::string ("Database");
+}
+
+ void FindPlayer(){
+     std::cout<<"find player";
+ }
+ char GetOpponentType(){
+     std::cout<<"GetOpponentType";
+ }     
+ bool IsPlayAgain(){}
+void Loop(){
+    std::cout<<"Loop";
+}
+bool is_playing()
