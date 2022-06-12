6e1739da44456b77ea985a4746d05203045e7ee9
Arisha75144 <88162494+Arisha75144@users.noreply.github.com>
2022-06-12 18:27:05 +0300

    Update Board.cpp

--- src/Board.cpp
+++ src/Board.cpp
@@ -1,5 +1,27 @@
 //
 // Created by Dmitry Morozov on 10/6/22.
 //
-
+#include <iostream>
 #include "Board.hpp"
+Board::Board () {
+   std::cout << "class Board constructor is running" << std::endl;
+
+}
+void Board::setPosition(int position, char mark){
+  std::cout<<"SetPosition"<<std::endl;
+}
+void Board::PrintBoard(){
+  std::cout<<"PrintBoard"<<std::endl;
+}
+
+void Board::ClearBoard(){
+  std::cout<<"ClearBoard"<<std::endl;
+}
+bool Board::isFull(){
+  std::cout<<"IsFull";
+  return True;
+}
+bool Board::isFree(int position){
+  std::cout<<"IsFree";
+  return True;
+}
