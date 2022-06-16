7ccc0d283d483fd294547452c673350773bff20f
Dmitry Morozov <9984398@gmail.com>
2022-06-16 22:19:47 +0300

    Pre_lesson : need fix

--- tests/unit_tests/test_Game.cpp
+++ tests/unit_tests/test_Game.cpp
@@ -6,3 +6,14 @@
 #include "gtest/gtest.h"
 
 
+TEST(Game, setNextPlayer) {
+//    Game game;
+//    game.setNextPlayer();
+//    EXPECT_EQ(game.getNextPlayer(), 1);
+//    game.setNextPlayer();
+//    EXPECT_EQ(game.getNextPlayer(), 2);
+//    game.setNextPlayer();
+//    EXPECT_EQ(game.getNextPlayer(), 1);
+//}
+
+}
