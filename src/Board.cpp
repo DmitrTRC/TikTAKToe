87d66441a26800ef66cc11b195715c4aa4aa2d63
Dev Spain <9984398@gmail.com>
2022-06-20 00:53:58 +0300

    Code review. New FIX needed.

--- src/Board.cpp
+++ src/Board.cpp
@@ -17,8 +17,8 @@ void Board::setPosition (int position, char mark) {
 }
 
 //TODO: Draw the board with the current state of the game
-//FIXME: positions_ : Member of class Board!
-void Board::PrintBoard () { // Look at method declaration!  void Board::PrintBoard ()
+//FIXME: cout : is the member of the std namespace. Use std::cout instead or using std::cout
+void Board::PrintBoard () { // Ok! Everything is correct
     cout << "    |     | \n "; //Better way to use std::endl.
     cout << positions_[1] << "  | " <<positions_[2] << "   | " << positions_[3] << " \n";
     cout << " ---|-----|--- \n";
@@ -42,6 +42,9 @@ void Board::ClearBoard () { // Fill positions with empty characters
 bool Board::isFull () {
     int count=0;
     for (int i=0;i<board.size();i++){
+        //FIXME:
+        //  board : is not a member of class Board . The size of the board is in the k_board_size constant
+        // You have to iterate over the positions_ array remembering to start from 1 and up to k_board_size included
         if (board[i]!=' ')
         count+=1;
     }
@@ -57,7 +60,9 @@ bool Board::isFull () {
 
 //TODO: Implement method
 bool Board::isFree (int position) {
+    //FIXME: We do not need iterate over the all positions_ array. Just check the position that we want to check
     for (int i=0;i<positions_.size();i++){
+        // FIXME: Check variable names!
         if (positions[position_]==' ')
         return true;
         else
