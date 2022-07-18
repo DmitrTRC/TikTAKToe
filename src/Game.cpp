1a2cbadc64d4576df7463438826f3a7e19274083
Arisha75144 <88162494+Arisha75144@users.noreply.github.com>
2022-07-18 15:11:09 +0200

    Update Game.cpp

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,6 +1,3 @@
-//
-// Created by Dmitry Morozov on 28/5/22.
-//
 #include <iostream>
 #include <stdlib.h>
 #include "Game.hpp"
@@ -10,7 +7,7 @@
 Game::Game () {
 
     std::cout << "class Game constructor is running" << std::endl;
-    srand (time (0));
+    srand (time (nullptr));
 
 }
 
@@ -19,72 +16,88 @@ Game::~Game () {
 
 }
 
+//Point 2. Main Game loop. Initialize the game.
 void Game::Start () {
     std::cout << "Game started" << std::endl;
 
-    HumanPlayer player1 (InputUserName (1));
+    //Point 3. Create two players. ( Ask for type of player and name of player)
+
+
+    HumanPlayer player1 (InputUserName ());
     players_.push_back (&player1); // Put player1 address in vector
 
-    HumanPlayer player2 (InputUserName (2));
-    players_.push_back (&player2); // Put player2 address in vector
+    HumanPlayer player2 (InputUserName ());
+    players_.push_back (&player2);
 
+    //Point 4. Set first player.
     current_player_ = getFirstPlayer ();
 
-//    auto db = LoadDataBase ();
+    players_[current_player_]->setMark ('X');
+    players_[(current_player_ + 1) % 2]->setMark ('O');
 
-// Repeating the game until the user quits
-    while (is_playing ()) {
-//
 
+    do {
         Loop ();
+    } while (IsPlayAgain ());
 
-        IsPlayAgain ();
-// End of the Game
-    }
 }
 
 
-std::string Game::InputUserName (int player_number) {
-    std::cout << "Enter User " << player_number << " name : ";
+Player &Game::getCurrentPlayer () {
+    return *players_[current_player_];
+}
+
+std::string Game::InputUserName () {
+    std::cout << "Enter Player name : ";
     std::string name;
     std::cin >> name;
     return name;
 }
 
-std::string Game::LoadDataBase () {
-    std::cout << "DataBase" << std::endl;
-    return "Database";
-}
-
-void Game::FindPlayer () {
-    std::cout << "find player";
-}
-
-char Game::GetOpponentType () {
-    std::cout << "GetOpponentType";
-    return 'X';
-}
 
 bool Game::IsPlayAgain () {
-    std::cout << "isPlayAgain";
-    return true;
-}
-
-void Game::Loop () {
-    std::cout << "Game Loop" << std::endl;
-    int cur_move = players_[current_player_]->engageMove ();
-    if (board_.isFree (cur_move)){
-        //TODO: Set the move in the board
+    std::cout << "Do you want to play again? (y/n)";
+    char answer;
+    std::cin >> answer;
+    if (answer == 'y') {
+        return true;
     } else {
-        std::cout << "Invalid move" << std::endl;
+        return false;
     }
-    if (isWinner()) {
-        std::cout << "Player " << current_player_ << " wins" << std::endl;
-
-    }
-    setNextPlayer ();
-
 }
+
+//Point 5.1. Main game logic loop.
+void Game::Loop () {
+    bool game_active = true;
+
+    while (game_active) {
+
+        board_.PrintBoard ();
+
+        std::cout << "Player " << getCurrentPlayer ().getName () << " turn" << std::endl;
+
+        int next_move = getCurrentPlayer ().engageMove (board_);
+
+        if (!board_.setPosition (next_move, getCurrentPlayer ().getMark ())) {
+            std::cout << "Invalid move" << std::endl;
+            continue;
+        }
+
+        //Point 5.5. Check if the game is over.
+        if (board_.isWinner ()) {
+            std::cout << "Player " << getCurrentPlayer ().getName () << " won!" << std::endl;
+            board_.PrintBoard ();
+            getCurrentPlayer ().addWin (); // Add one win to the player score_
+            game_active = false;
+        } else if (board_.isFull ()) {
+            std::cout << "Draw!" << std::endl;
+            game_active = false;
+        } else {
+            setNextPlayer ();
+        }
+    }
+}
+
 
 bool Game::is_playing () {
     std::cout << "is_playing" << std::endl;
@@ -95,7 +108,6 @@ bool Game::is_playing () {
 int Game::getFirstPlayer () {
     return rand () % 2; // Native C function to generate random number
 }
-
 
 
 void Game::setNextPlayer () { //current_player ( 0 or 1 )
