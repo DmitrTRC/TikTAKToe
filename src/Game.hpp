d6f88aaabdc36f94e34e66ea9dd7190442f43499
Arisha75144 <88162494+Arisha75144@users.noreply.github.com>
2022-07-18 15:14:12 +0200

    Update Game.hpp

--- src/Game.hpp
+++ src/Game.hpp
@@ -6,32 +6,35 @@
 #include "Board.hpp"
 #include "player.hpp"
 
-//TODO: Add method int getFirstPlayer() to class Game. So that it can return index of first player.
+
 class Game {
 public:
     Game (); // default constructor
     ~Game ();
 
-    //Todo: Fix! Ты не правильно пишешь реализацию методов в классе Game! Используй scope resolution operator ::
-    void SetSecondPlayer();
-    void FindPlayer();
-    char GetOpponentType();
-    bool IsPlayAgain();
-    bool is_playing();
-    int getFirstPlayer();
-    void setNextPlayer();
-    bool isWinner(){};
-    
+    static bool IsPlayAgain ();
 
+    static bool is_playing ();
+
+    static int getFirstPlayer ();
+
+    void setNextPlayer ();
 
     void Start (); // Game entry point
+
 private:
-    bool is_playing_; // flag to check if the game is still running
-    std::vector<Player*> players_; // vector of players
-    std::string LoadDataBase();
-    std::string InputUserName (int); // function to get user name
-    int current_player_; // index of current player
+    bool is_playing_{}; // flag to check if the game is still running
+    std::vector<Player *> players_; // vector of players
+
+
+
+
+    std::string InputUserName (); // function to get user name
+    int current_player_{}; // index of current player
     Board board_; // game board
+
+
+    Player &getCurrentPlayer ();
 
     void Loop ();
 };
