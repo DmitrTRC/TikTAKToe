a27ecc1e46f893bf400aae81631361bc58999450
Dev Spain <9984398@gmail.com>
2022-07-21 20:45:40 +0300

    class Score : draft implemented.

--- src/Result.hpp
+++ src/Result.hpp
@@ -5,13 +5,26 @@
 #ifndef TICTAC_RESULT_HPP
 #define TICTAC_RESULT_HPP
 
+#include <iostream>
+
+
+class Score {
+    std::string name;
+    int wins;
+    int loses;
+    int draws;
+
+};
 
 class Result {
 public:
-    void ShowResult();
-    bool LoadDB();
-    bool SaveDB();
-    void ShowScoreTable();
+    void ShowResult ();
+
+    bool LoadDB ();
+
+    bool SaveDB ();
+
+    void ShowScoreTable ();
 
 
 };
