6c9cd395024728fb575f02e7c76a4ce909a3abb2
dmitrymorozov <morozovd@me.com>
2022-08-07 21:58:19 +0300

    AI Need Sleep Fix

--- src/Ai_Easy_Player.cpp
+++ src/Ai_Easy_Player.cpp
@@ -14,5 +14,7 @@ AI_EASY_Player::AI_EASY_Player (const std::string &name) {
 int AI_EASY_Player::engageMove (const Board &board) {
     // Return random number from available positions
     //TODO: implement AI logic : iterate through available positions and return if it has a winning move or randome move
+    std::cout << "Thinking ..." << std::endl;
+    //sleep (1);
     return board.getAvailablePositions ().at (rand () % board.getAvailablePositions ().size ());
 }
