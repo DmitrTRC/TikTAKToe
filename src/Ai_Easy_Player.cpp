ec737317003cc002e9f4478d6dc94ae2fb0c7b9b
Dev Spain <9984398@gmail.com>
2022-08-07 22:18:18 +0300

    Thinking timer : added for AI

--- src/Ai_Easy_Player.cpp
+++ src/Ai_Easy_Player.cpp
@@ -4,6 +4,8 @@
 
 #include <vector>
 #include "Ai_Easy_Player.hpp"
+#include <chrono>
+#include <thread>
 
 
 AI_EASY_Player::AI_EASY_Player (const std::string &name) {
@@ -14,7 +16,11 @@ AI_EASY_Player::AI_EASY_Player (const std::string &name) {
 int AI_EASY_Player::engageMove (const Board &board) {
     // Return random number from available positions
     //TODO: implement AI logic : iterate through available positions and return if it has a winning move or randome move
+
     std::cout << "Thinking ..." << std::endl;
-    //sleep (1);
+
+    // Sleep for 1 second
+    std::this_thread::sleep_for (std::chrono::seconds (1));
+
     return board.getAvailablePositions ().at (rand () % board.getAvailablePositions ().size ());
 }
