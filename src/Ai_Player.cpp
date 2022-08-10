dac2bb1e95b547401ebed54da3c99577391b6e66
Dev Spain <9984398@gmail.com>
2022-08-11 01:15:03 +0300

    NC1 AI_Player : Start developing

--- src/Ai_Player.cpp
+++ src/Ai_Player.cpp
@@ -2,6 +2,8 @@
 // Created by Dmitry Morozov on 18/7/22.
 //
 
+#include <vector>
+
 #include "Ai_Player.hpp"
 
 
@@ -11,8 +13,35 @@
 //Step 4: Check available positions to win by diagonal
 //Step 5: Check positions [2,4,6,8] to win by diagonal
 int AI_Player::engageMove (const Board &board) {
+    //Step 1: Check available positions to win by move
+    //Step 2: Check available positions to block opponent from winning by move
+    //Step 3: Check available positions in place 5
+    //Step 4: Check available positions to win by diagonal
+    //Step 5: Check positions [2,4,6,8] to win by diagonal
+    int result_move = -1;
+    auto *board_ptr = new Board (board);
+    std::vector<int> available_positions = board.getAvailablePositions ();
 
-    return 0;
+    for (int position : available_positions) {
+        board_ptr->setPosition (position, getMark ());
+        if (board_ptr->isWinner ()) {
+            result_move = position;
+            break;
+        }
+    }
+
+    if (result_move == -1) {
+        for (int position : available_positions) {
+            board_ptr->setPosition (position, getOpponentMark ());
+            if (board_ptr->isWinner ()) {
+                result_move = position;
+                break;
+            }
+        }
+    }
+
+
+    return result_move;
 }
 
 AI_Player::AI_Player (const std::string &name) {
